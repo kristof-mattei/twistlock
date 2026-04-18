36ec63fbb2df5947d2822dc0b6359cc8cb05ffe8
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-04-17 17:52:03 -0700

    fix: added tests for filters

--- src/endpoints/containers.rs
+++ src/endpoints/containers.rs
@@ -62,3 +62,47 @@ impl ApiEndpoint for RestartContainer {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use hashbrown::{HashMap, HashSet};
+    use pretty_assertions::assert_eq;
+
+    use crate::client::url_encode;
+    use crate::filters::{Filters, Health};
+
+    fn build(mode: &str) -> Filters {
+        Filters {
+            health: Some(HashSet::from_iter([Health::Unhealthy])),
+            label: Some(HashMap::from_iter([(
+                Box::from(mode),
+                Some(Box::from("true")),
+            )])),
+            ..Filters::default()
+        }
+    }
+
+    #[test]
+    fn build_decode_autoheal() {
+        let something_and_unhealthy = build("something");
+
+        let something_and_unhealthy_encoded = url_encode(&something_and_unhealthy).unwrap();
+
+        assert_eq!(
+            &*something_and_unhealthy_encoded,
+            "%7B%22label%22%3A%5B%22something%3Dtrue%22%5D%2C%22health%22%3A%5B%22unhealthy%22%5D%7D"
+        );
+    }
+
+    #[test]
+    fn build_decode_custom() {
+        let custom_and_unhealthy = build("custom");
+
+        let custom_and_unhealthy_encoded = url_encode(&custom_and_unhealthy).unwrap();
+
+        assert_eq!(
+            &*custom_and_unhealthy_encoded,
+            "%7B%22label%22%3A%5B%22custom%3Dtrue%22%5D%2C%22health%22%3A%5B%22unhealthy%22%5D%7D"
+        );
+    }
+}
