508f72cbad2d4e57e829e37f2e26e87fe07efb87
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-09-17 12:05:57 -0700

    feat(client)!: take `ContainerRef` and `NetworkRef` in the ID-taking methods

--- src/models/id.rs
+++ src/models/id.rs
@@ -30,6 +30,26 @@ impl std::fmt::Display for ContainerId {
     }
 }
 
+pub enum ContainerRef<'r> {
+    Id(&'r ContainerId),
+    IdOrName(&'r str),
+}
+
+impl<'r> ContainerRef<'r> {
+    pub(crate) fn as_str(&self) -> &'r str {
+        match *self {
+            ContainerRef::Id(id) => id.as_str(),
+            ContainerRef::IdOrName(id_or_name) => id_or_name,
+        }
+    }
+}
+
+impl<'r> From<&'r ContainerId> for ContainerRef<'r> {
+    fn from(id: &'r ContainerId) -> Self {
+        ContainerRef::Id(id)
+    }
+}
+
 #[derive(Deserialize, Debug, PartialEq, Eq, Hash)]
 pub struct NetworkId(Box<str>);
 
@@ -53,6 +73,26 @@ impl NetworkId {
 impl std::fmt::Display for NetworkId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&self.0)
+    }
+}
+
+pub enum NetworkRef<'r> {
+    Id(&'r NetworkId),
+    IdOrName(&'r str),
+}
+
+impl<'r> NetworkRef<'r> {
+    pub(crate) fn as_str(&self) -> &'r str {
+        match *self {
+            NetworkRef::Id(id) => id.as_str(),
+            NetworkRef::IdOrName(id_or_name) => id_or_name,
+        }
+    }
+}
+
+impl<'r> From<&'r NetworkId> for NetworkRef<'r> {
+    fn from(id: &'r NetworkId) -> Self {
+        NetworkRef::Id(id)
     }
 }
 
